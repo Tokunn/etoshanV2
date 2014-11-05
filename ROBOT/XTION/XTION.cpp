54d160896fc49997c3b8c7be1a988373dad87e0c
Tokunn <kaseikappatokunn2@yahoo.co.jp>
2014-11-05 18:34:12 +0900

    zero

--- ROBOT/XTION/XTION.cpp
+++ ROBOT/XTION/XTION.cpp
@@ -14,34 +14,55 @@ class DepthSensor
             if ( ret != openni::STATUS_OK ) {
                 throw std::runtime_error( "openni::Device::open() failed." );
             }
-
-            colorStream.create( device, openni::SENSOR_COLOR );
-            colorStream.start();
+            depthStream.create( device, openni::SENSOR_DEPTH );
+            depthStream.start();
         }
 
         void update() {
-            openni::VideoFrameRef colorFrame;
-            colorImage = showColorStream( colorFrame );
-            cv::imshow( "Color Stream", colorImage );
+            openni::VideoFrameRef depthFrame;
+            depthStream.readFrame( &depthFrame );
+            depthImage = showDepthStream( depthFrame );
+            cv::imshow( "Depth Stream", depthImage );
         }
 
     private:
 
-        cv::Mat showColorStream( const openni::VideoFrameRef& colorFrame ) {
-            cv::Mat colorImage = cv::Mat( colorFrame.getHeight(),
-                                          colorFrame.getWidth(),
-                                          CV_8UC3,
-                                          (unsigned char*)colorFrame.getData()
-                    );
-            cv::cvtColor( colorImage, colorImage, CV_RGB2BGR );
+        cv::Mat showDepthStream( const openni::VideoFrameRef& depthFrame ) {
+            cv::Mat depthImage = cv::Mat( depthFrame.getHeight(), depthFrame.getWidth(), CV_16UC1, (unsigned short*)depthFrame.getData() );
+            depthImage.convertTo( depthImage, CV_8U, 255.0 / 10000 );
+            showCenterDistance( depthImage, depthFrame );
+            return depthImage;
+        }
 
-            return colorImage;
+        void showCenterDistance( cv::Mat& depthImage, const openni::VideoFrameRef& depthFrame ) {
+            openni::VideoMode videoMode = depthStream.getVideoMode();
+
+            int centerX = videoMode.getResolutionX() / 2;
+            int centerY = videoMode.getResolutionY() / 2;
+            int centerIndex = ( centerY * videoMode.getResolutionX()) + centerX;
+
+            unsigned short* depth = (unsigned short*)depthFrame.getData();
+
+            std::stringstream ss;
+            ss << "Center Point :" << depth[centerIndex];
+            cv::putText( depthImage, ss.str(), cv::Point( 0, 50 ), cv::FONT_HERSHEY_SIMPLEX, 1.0, cv::Scalar( 255 ) );
+
+            unsigned short min = 10000;
+            for (int i = 0; i < 320; i++) {
+                if (depth[i] == 0) {
+                    std::cout << depth[i] << '\n';
+                }
+                else if (min > depth[i]) {
+                    min = depth[i];
+                }
+            }
+            std::cout << "Min : " << min << "\n\n";
         }
 
         openni::Device device;
-        openni::VideoStream colorStream;
+        openni::VideoStream depthStream;
 
-        cv::Mat colorImage;
+        cv::Mat depthImage;
 };
 
 int main( int argc, const char * argv[] ) {
