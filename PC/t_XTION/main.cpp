1afa087691f477c96e6a4fba9456504db34e8450
Tokunn <kaseikappatokunn2@yahoo.co.jp>
2014-12-06 16:43:00 +0900

    delete \n

--- PC/t_XTION/main.cpp
+++ PC/t_XTION/main.cpp
@@ -40,13 +40,13 @@
 
 
 enum Pose{ NONE=0, MAJOKO=1, OBAKE=2, KAIDAN=3, NEKO=4, KING=5, BRUNA=6 };
-const char pose_char[][8] = { "NONE__\n",
-                        "MAJOKO\n",
-                        "OBAKE_\n",
-                        "KAIDAN\n",
-                        "NEKO__\n",
-                        "KING__\n",
-                        "BRUNA_\n"  };
+const char pose_char[][] = { "NONE__",
+                        "MAJOKO",
+                        "OBAKE_",
+                        "KAIDAN",
+                        "NEKO__",
+                        "KING__",
+                        "BRUNA_"  };
 
 
 /******* Xtion Class *******/
@@ -222,11 +222,15 @@ void Xtion::sendSerial( Pose pose )
     int fd;
     int return_write;
     fd = open(SERIAL_PORT, O_RDWR);
-    return_write = write(fd, pose_char[pose], 7);
+    return_write = write(fd, pose_char[pose], 6);
     if (return_write < 0) {
         std::cout << "Send serial Error\n";
     }
-    close(fd);
+    return_write = write(fd, '\n', 1);
+    if (return_write < 0) {
+        std::cout << "Send serial Error\n";
+    }
+   close(fd);
 }
 
 
