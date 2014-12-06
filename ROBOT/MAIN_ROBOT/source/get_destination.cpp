b850a219e72311bd11773e0988bc87779a2db5ac
Tokunn <kaseikappatokunn2@yahoo.co.jp>
2014-12-06 22:54:49 +0900

    home

--- ROBOT/MAIN_ROBOT/source/get_destination.cpp
+++ ROBOT/MAIN_ROBOT/source/get_destination.cpp
@@ -46,7 +46,6 @@ int Destination::comv_destination_string2int(
         const std::string* pose_string, const std::string* string_destination) {
     std::cout << ">'" << *string_destination << "'<" << '\n';     // #=# DEBUG #=#
     const char *char_destination = string_destination->c_str();
-    //unsigned int size = sizeof(pose_string) / sizeof(int);
     for (unsigned int i = 0; i < 7; i++) {
         const char *pose_char = pose_string[i].c_str();
         if (char_destination[3] == pose_char[3]) {
